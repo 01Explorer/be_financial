fa4e8ba2250814934480df3ebfadc4d107089754
Gabriel Santos <gabrielsantostca@gmail.com>
2023-01-04 16:37:13 -0300

    style(app): set the initial theme for the app development

---
+++ lib/views/themes/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:be_financial/views/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  final ThemeData standardTheme = ThemeData(
+    scaffoldBackgroundColor: Palette.backgroundGrey,
+    fontFamily: GoogleFonts.ibmPlexSans().fontFamily,
+  );
+}
