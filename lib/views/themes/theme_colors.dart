036d7a9c1b63f1d1d5b25e9c3926842eab1dcc69
Gabriel Santos <gabrielsantostca@gmail.com>
2023-01-04 16:36:39 -0300

    style(app): initial pallette of colors that will be used

---
+++ lib/views/themes/theme_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color darkBlue = Color(0xFF4E3BBB);
+  static const Color yellow = Color(0xFFE7BE1A);
+  static const Color teal = Color(0xFF389BBA);
+  static const Color whiteDetails = Color(0xFFEAF3FA);
+  static const Color backgroundGrey = Color(0xFFEEECF9);
+} 
